4dc32f1e843ca8bed2e1485bd23922ce3fbd51ea
Sebastian Malton <sebastian@malton.name>
2022-08-17 09:11:02 -0400

    Switch to lodash uniqby
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/handlers.ts
+++ src/handlers.ts
@@ -2,7 +2,7 @@ import * as aesprim from '../upstream/aesprim';
 import * as ESTree from 'estree';
 
 import { slice } from './slice';
-import { uniq as _uniq } from 'underscore';
+import uniqBy from "lodash.uniqby";
 import { JSONPath } from '.';
 import _evaluate from 'static-eval';
 
@@ -58,7 +58,7 @@ function traverser(recurse?) {
  * @hidden
  */
 function evaluate(ast: ESTree.Expression, vars: Record<string,any>) {
-  try { 
+  try {
     return _evaluate(ast, vars);
   } catch (e) {
   }
@@ -91,7 +91,7 @@ function _traverse(passable) {
 function unique(results : any[]) {
   results = results.filter(d => d);
 
-  return _uniq(
+  return uniqBy(
     results,
     r => r.path.map(function(c) { return String(c).replace('-', '--') }).join('-')
   );
@@ -165,31 +165,31 @@ export class Handlers {
         return [ { value: value[key], path: partial.path.concat(key) } ]
       }
     },
-  
+
     'member-descendant-identifier':
       _traverse(function(key, value, ref) { return key == ref }),
-  
+
     'subscript-child-numeric_literal':
       _descend(function(key, value, ref) { return key === ref }),
-  
+
     'member-child-numeric_literal':
       _descend(function(key, value, ref) { return String(key) === String(ref) }),
-  
+
     'subscript-descendant-numeric_literal':
       _traverse(function(key, value, ref) { return key === ref }),
-  
+
     'member-child-wildcard':
       _descend(function() { return true }),
-  
+
     'member-descendant-wildcard':
       _traverse(function() { return true }),
-  
+
     'subscript-descendant-wildcard':
       _traverse(function() { return true }),
-  
+
     'subscript-child-wildcard':
       _descend(function() { return true }),
-  
+
     'subscript-child-slice': function(component, partial) {
       if (is_array(partial.value)) {
         var args = component.expression.value.split(':').map(_parse_nullable_int);
@@ -197,7 +197,7 @@ export class Handlers {
         return slice.apply(null, [values].concat(args));
       }
     },
-  
+
     'subscript-child-union': function(component, partial) {
       var results = [];
       component.expression.value.forEach(function(component) {
@@ -208,16 +208,16 @@ export class Handlers {
           results = results.concat(_results);
         }
       }, this);
-  
+
       return unique(results);
     },
-  
+
     'subscript-descendant-union': function(component, partial, count) {
       var self = this;
-  
+
       var results = [];
       var nodes = JSONPath.nodes(partial, '$..*').slice(1);
-  
+
       nodes.forEach(function(node) {
         if (results.length >= count) return;
         component.expression.value.forEach(function(component) {
@@ -227,47 +227,47 @@ export class Handlers {
           results = results.concat(_results);
         });
       });
-  
+
       return unique(results);
     },
-  
+
     'subscript-child-filter_expression': function(component, partial, count) {
-  
+
       // slice out the expression from ?(expression)
       var src = component.expression.value.slice(2, -1);
       var ast = aesprim.parse(src).body[0].expression;
-  
+
       var passable = function(key, value) {
         return evaluate(ast, { '@': value });
       }
-  
+
       return this.descend(partial, null, passable, count);
-  
+
     },
-  
+
     'subscript-descendant-filter_expression': function(component, partial, count) {
-  
+
       // slice out the expression from ?(expression)
       var src = component.expression.value.slice(2, -1);
       var ast = aesprim.parse(src).body[0].expression;
-  
+
       var passable = function(key, value) {
         return evaluate(ast, { '@': value });
       }
-  
+
       return this.traverse(partial, null, passable, count);
     },
-  
+
     'subscript-child-script_expression': function(component, partial) {
       var exp = component.expression.value.slice(1, -1);
       return eval_recurse(partial, exp, '$[{{value}}]');
     },
-  
+
     'member-child-script_expression': function(component, partial) {
       var exp = component.expression.value.slice(1, -1);
       return eval_recurse(partial, exp, '$.{{value}}');
     },
-  
+
     'member-descendant-script_expression': function(component, partial) {
       var exp = component.expression.value.slice(1, -1);
       return eval_recurse(partial, exp, '$..value');
@@ -280,7 +280,7 @@ export class Handlers {
 
     this._fns['subscript-child-string_literal'] =
       this._fns['member-child-identifier'];
-    
+
     this._fns['member-descendant-numeric_literal'] =
       this._fns['subscript-descendant-string_literal'] =
       this._fns['member-descendant-identifier'];
@@ -290,7 +290,7 @@ export class Handlers {
   resolve(component) {
     var key = [ component.operation, component.scope, component.expression.type ].join('-');
     var method = this._fns[key];
-  
+
     if (!method) throw new Error("couldn't resolve key: " + key);
     return method.bind(this);
   }
@@ -299,7 +299,7 @@ export class Handlers {
     if (!(handler instanceof Function)) {
       throw new Error("handler must be a function");
     }
-  
+
     this._fns[key] = handler;
   }
 }
